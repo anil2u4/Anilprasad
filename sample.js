b41b76d91d00b8ed7ccfad5f04359abdc558dd6e
anil2u4 <63222005+anil2u4@users.noreply.github.com>
2025-09-28 01:01:18 -0700

    Add files via upload

---
+++ sample.js
@@ -0,0 +1,11 @@
+    function handleClick()
+        {
+            document.querySelector("body > button").innerText="neethu";
+            document.getElementById("heading").style.color="purple";
+        }
+
+        function display() 
+        {
+            var text=document.getElementById("inp").value
+            document.getElementById("head").innerHTML=text
+        }
